7446e3a1637748f486144a5a00d0665ae5b5d51b
ardhiansyah <ardhimetanaputra@gmail.com>
2018-03-28 11:24:10 +0700

    update event on

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,11 @@
 
       <div class="phone-viewport">
         <md-list>
-          <md-list-item @click.native="toggleSideNavLeft('/')">
+          <md-list-item @click="toggleSideNavLeft('/')">
             <md-icon>home</md-icon>
             <span>Home</span>
           </md-list-item>
-          <md-list-item @click.native="toggleSideNavLeft('/post')">
+          <md-list-item @click="toggleSideNavLeft('/post')">
             <md-icon>add</md-icon>
             <span>New Post</span>
           </md-list-item>
