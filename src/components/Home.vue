a68eeed4f5074607ca1b5eb9d77c4a42007db542
herby herado <herby.herado@gmail.com>
2018-03-28 11:30:16 +0700

    done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <h1>Home</h1>
     <md-card v-for="cat in latestCats" :key="cat['.key']">
       <md-card-media>
-        <img src="cat.url" :alt="cat.comment">
+        <img :src="cat.url" :alt="cat.comment">
       </md-card-media>
 
       <md-card-header>
@@ -11,7 +11,7 @@
       </md-card-header>
 
       <md-card-actions>
-        <router-link to="'/detail/'+cat['.key']">
+        <router-link :to="{ name: 'Detail', params: { id: cat['.key'] }}">
           <md-button>Details</md-button>
         </router-link>
       </md-card-actions>
@@ -21,12 +21,14 @@
 
 <script>
 export default {
-  firebase: {
-    cats: this.$db.ref('cats')
+  firebase () {
+    return {
+      cats: this.$db.ref('cats')
+    }
   },
   computed: {
     latestCats: function () {
-      this.cats.reverse()
+      return this.cats.reverse()
     }
   }
 }
