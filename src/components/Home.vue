4ebcc716265ea3fec7c7a2f9a22701f0071259e8
ardhiansyah <ardhimetanaputra@gmail.com>
2018-03-28 10:58:57 +0700

    fix frontend

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,7 +11,7 @@
       </md-card-header>
 
       <md-card-actions>
-        <router-link to="'/detail/'+cat['.key']">
+        <router-link :to="'/detail/'+cat['.key']">
           <md-button>Details</md-button>
         </router-link>
       </md-card-actions>
@@ -22,13 +22,13 @@
 <script>
 export default {
   firebase () {
-    {
-      cats: this.$db.ref('cats');
+    return {
+      cats: this.$db.ref('cats')
     }
   },
   computed: {
-    latestCats: {
-      this.cats.reverse()
+    latestCats () {
+      return this.cats.reverse()
     }
   }
 }
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -43,12 +43,12 @@ export default {
         detail: ''
       },
       loading: true,
-      err: false,
+      err: false
     }
   },
   methods: {
-    getCat() {
-      this.$http.get('http://random.cat/meow')
+    getCat () {
+      this.$http.get('http://aws.random.cat/meow')
         .then((response) => {
           this.randomCat.url = response.data.file
           setTimeout(() => { this.loading = false }, 1000)
@@ -57,14 +57,14 @@ export default {
           this.err = err
         })
     },
-    postCat() {
+    postCat () {
       console.log('postCat ', this.randomCat)
       this.$db.ref('cats').push(this.randomCat, () => {
-        this.$router.push('/');
+        this.$router.push('/')
       })
     }
   },
-  mounted: {
+  mounted () {
     this.getCat()
   }
 }
