8d7bcbca4a38c35c56c829d7bf22e07a78dea34e
ardhiansyah <ardhimetanaputra@gmail.com>
2018-03-28 11:15:48 +0700

    fix bind url

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <h1>Home</h1>
     <md-card v-for="cat in latestCats" :key="cat['.key']">
       <md-card-media>
-        <img src="cat.url" :alt="cat.comment">
+        <img :src="cat.url" :alt="cat.comment">
       </md-card-media>
 
       <md-card-header>
